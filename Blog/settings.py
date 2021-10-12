332944b8488cfb1b41a8d9b680879925ffa40a40
karlaVane <kmoyonr@est.ups.edu.ec>
2021-10-12 03:46:30 -0500

    Preparando para el despliegue

--- Blog/settings.py
+++ Blog/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-9%(pge4ajx)o(c@r52n+!vl+su8q81y_h69wi@nlpwtp%tzvy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
